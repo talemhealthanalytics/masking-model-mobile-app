5fa9188fed21391b11fa281d17448cc2dce38bdd
Rohit Singh <rkochhar@uwaterloo.ca>
2022-01-12 17:23:44 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # masking-model-mobile-app
-Repository containing code and documentation for masking model mobile application
+
+The code in this repository is adapted from the code published by Dennis Ippel in this [repository](https://github.com/MasDennis/SegmentationAndOcclusion), and outlined in this [Medium article](https://rozengain.medium.com/using-coreml-in-arkit-for-object-segmentation-and-occlusion-988a6c7e18dd).
+
+## Product Overview
+The goal of the *masking-model-mobile-app* is to run image processing models on the live video captured by an iPhone camera. This project was started with the ultimate goal of being able to run the masking model (found [here](https://github.com/talemhealthanalytics/MaskRCNN)) on live video captured on the user's iPhone. 
+
+## Technical Overview
+![App flowchart](./flowchart.png?raw=true "App Flowchart")
+This app uses Swift and Apple CoreML tools to run a user-defined machine learning model on device. By using the **vision** library, the app is able to interface the video collected on the device, running the model in real-time. 
+
+Two models are used in this code, the first is used for object detection and the second is used for segmentation. The object detection is done using a *YOLOv3Int8LUT* model, and the second one should *ideally* be our masking model. **This is the only remaining piece of the puzzle that would complete this project, and the remaining blocker is highlighted below in the blockers section**. For the time being, a *DeepLabV3Int8Image* model is used for segmentation instead. As a result, the app segments entire cars rather than just the damaged region of interest.
+
+First, the view is loaded and a request to load the camera feed is sent. If successful, the app will then call `objectDetectionRequest` on the video feed, acting on each frame collected from the video. This function accesses the *YOLOv3Int8LUT* and is functional in it's current state.
+
+Next, if an object is found, `segmentationRequest` is called on the detected object, segmenting the detected vehicle and occluding it from the outputted video to the user (This causes a reduction in FPS and causes the app to become bloated around 30FPS). If no object is found, the object detection request is continued. This function currently accesses the *DeepLabV3Int8Image*, and must be replaced with our masking model to be considered complete.
+
+## Developer instructions
+- First, the latest version of XCode must be installed, which can be done by following [these instructions](https://mac.install.guide/commandlinetools/3.html). 
+- Once this repository is pulled, open Xcode and select "Open an existing project".
+- Navigate to the directory in which the `.Xcodeproj` can be found, and select this file. This will open the Xcode workspace where all development will be done.
+- To load the app onto an iPhone, connect the mobile device to your computer and select it from the dropdown menu at the top pane of the editor, and press the play button on the left hand side of the editor. 
+- Note that simulators will not work with this model, as it must access the device's camera which is not possible for simulated devices.s
+
+## Blockers
+The main blocker to the completion of this app is plugging in our masking model instead of the *DeepLabV3Int8Image* model. As our model is a segmentation model which would segment the damaged region of interest of a car rather than the car entirely, which is what the app currently does. 
+
+The blocker is caused by the architecture of our masking model, which prevents us from transforming the model from a TensorFlow model to an MLModel. The following approaches were tried:
+- First, we tried converting the model from its `.h5` representation to an MLModel. However, this failed as the model is written in TensorFlow 1 rather than TensorFlow 2.
+- Second, we tried converting the model from its `.pb` and `.graph` file to an MLModel, as is recommended for TensorFlow 1 models, however these formats were never available with the masking model, and attempts to create them have been postponed until a valid use case that would prioritize the masking model is found.
---
+++ SegmentationAndOcclusion/ViewController.swift
@@ -0,0 +1,242 @@
+//
+//  ViewController.swift
+//  SegmentationAndOcclusion
+//
+//  Created by Dennis Ippel on 07/10/2020.
+//  Adaped by Rohit Singh for THA on Tue Oct 26, 2021
+//
+
+// Imports ------------------------------------------------
+import UIKit
+import Vision           // Vision is used for detection, classification and segmentation
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    private var screenSize: CGSize!
+    private var textureCache: CVMetalTextureCache?
+    
+    // Deeplab labels
+    private let labels = ["background", "aeroplane", "bicycle", "bird", "board", "bottle", "bus", "car", "cat", "chair", "cow", "diningTable", "dog", "horse", "motorbike", "person", "pottedPlant", "sheep", "sofa", "train", "tvOrMonitor"]
+    private let targetObjectLabel = "car"
+    private var targetObjectLabelindex: UInt { return UInt(labels.firstIndex(of: targetObjectLabel) ?? 0) }
+    
+    private weak var quadNode: SegmentationMaskNode?
+    private weak var highlighterNode: SCNNode?
+    
+    override var shouldAutorotate: Bool { return false }
+    
+    // --------------------------------------------------------------------------------
+    // Function Name: segmentationRequest
+    //
+    // Function inputs:
+    // Function outputs:
+    //
+    // Function description:
+    //
+    // --------------------------------------------------------------------------------
+    lazy var segmentationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: DeepLabV3Int8Image(configuration: MLModelConfiguration()).model)
+            let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+                self?.processSegmentations(for: request, error: error)
+            }
+            request.imageCropAndScaleOption = .scaleFill
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model.")
+        }
+    }()
+    
+    // --------------------------------------------------------------------------------
+    // Function Name: objectDetectionRequest
+    //
+    // Function inputs:
+    // Function outputs:
+    //
+    // Function description:
+    //
+    // --------------------------------------------------------------------------------
+    lazy var objectDetectionRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: YOLOv3Int8LUT(configuration: MLModelConfiguration()).model)
+            let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+                self?.processObjectDetections(for: request, error: error)
+            }
+            request.imageCropAndScaleOption = .scaleFill
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model.")
+        }
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        
+        screenSize = UIScreen.main.bounds.size
+        
+        let quadNode = SegmentationMaskNode(sceneView: sceneView)
+        quadNode.classificationLabelIndex = targetObjectLabelindex
+        sceneView.scene.rootNode.addChildNode(quadNode)
+        
+        self.quadNode = quadNode
+        
+        let highlighterNode = HighlighterNode()
+        highlighterNode.isHidden = true
+        sceneView.scene.rootNode.addChildNode(highlighterNode)
+        
+        self.highlighterNode = highlighterNode
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        resetTracking()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    private func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = []
+        configuration.environmentTexturing = .automatic
+        sceneView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
+    }
+    
+    private func processSegmentations(for request: VNRequest, error: Error?) {
+        guard error == nil else {
+            print("Segmentation error: \(error!.localizedDescription)")
+            return
+        }
+        
+        guard let observation = request.results?.first as? VNPixelBufferObservation else { return }
+
+        // The kCVPixelBufferMetalCompatibilityKey needs to be set if we want
+        // to use the texture in a Metal shader.
+        let pixelBuffer = observation.pixelBuffer.copyToMetalCompatible()!
+        
+        if textureCache == nil && CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, MTLCreateSystemDefaultDevice()!, nil, &textureCache) != kCVReturnSuccess {
+            assertionFailure()
+            return
+        }
+
+        var segmentationTexture: CVMetalTexture?
+
+        let result = CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                               textureCache.unsafelyUnwrapped,
+                                                               pixelBuffer,
+                                                               nil,
+                                                               .r8Uint,
+                                                               CVPixelBufferGetWidth(pixelBuffer),
+                                                               CVPixelBufferGetHeight(pixelBuffer),
+                                                               0,
+                                                               &segmentationTexture)
+
+        guard result == kCVReturnSuccess,
+              let image = segmentationTexture,
+              let texture = CVMetalTextureGetTexture(image)
+        else { return }
+
+        quadNode?.segmentationTexture = texture
+    }
+    
+    private func processObjectDetections(for request: VNRequest, error: Error?) {
+        guard error == nil else {
+            print("Object detection error: \(error!.localizedDescription)")
+            return
+        }
+        
+        guard var objectObservations = request.results as? [VNRecognizedObjectObservation] else { return }
+        
+        objectObservations.sort {
+            return $0.labels.firstIndex(where: { $0.identifier == targetObjectLabel }) ?? -1 < $1.labels.firstIndex(where: { $0.identifier == targetObjectLabel }) ?? -1
+        }
+        
+        guard let bestObservation = objectObservations.first,
+              let index = bestObservation.labels.firstIndex(where: { $0.identifier == targetObjectLabel }),
+              index < 3
+        else { return }
+        
+        // Enlarge the region of interest slightly to get better results
+        let enlargedRegionOfInterest = bestObservation.boundingBox.insetByNormalized(d: -0.02)
+
+        segmentationRequest.regionOfInterest = enlargedRegionOfInterest
+        quadNode?.regionOfInterest = enlargedRegionOfInterest
+        placeHighlighterNode(atCenterOfBoundingBox: enlargedRegionOfInterest)
+    }
+    
+    private func placeHighlighterNode(atCenterOfBoundingBox boundingBox: CGRect) {
+        guard let displayTransform = sceneView.session.currentFrame?.displayTransform(for: .landscapeLeft, viewportSize: screenSize) else { return }
+        
+        let viewBoundingBox = viewSpaceBoundingBox(fromNormalizedImageBoundingBox: boundingBox,
+                                                   usingDisplayTransform: displayTransform)
+        
+        let midPoint = CGPoint(x: viewBoundingBox.midX,
+                               y: viewBoundingBox.midY)
+
+        // Get the feature point that is closed to our detected rectangle's center
+        let results = sceneView.hitTest(midPoint, types: .featurePoint)
+        
+        guard let result = results.first else { return }
+        
+        let translation =  result.worldTransform.columns.3
+        highlighterNode?.simdWorldPosition = simd_float3(translation.x, translation.y, translation.z)
+        highlighterNode?.isHidden = false
+        
+        guard let camera = sceneView.pointOfView?.camera else { return }
+        
+        // Calculate the z buffer value so we can use it in the fragment shader.
+        // For the sake of occulsion, we need to write this value into the depth buffer.
+        // This will happen in the fragment shader.
+        quadNode?.depthBufferZ = calculateFragmentDepth(usingCamera: camera,
+                                                        distanceToTarget: Double(result.distance),
+                                                        usesReverseZ: sceneView.usesReverseZ)
+    }
+    
+    private func calculateFragmentDepth(usingCamera camera: SCNCamera, distanceToTarget: Double, usesReverseZ: Bool) -> Float {
+        // SceneKit uses a reverse z buffer since iOS 13. In case it uses a reverse buffer.
+        // We'll need to swap the near and far planes.
+        let zFar = usesReverseZ ? camera.zNear : camera.zFar
+        let zNear = usesReverseZ ? camera.zFar : camera.zNear
+        let range = 2.0 * zNear * zFar
+        // The depth value in in normalized device coordinates [-1, 1].
+        let fragmentDepth = (zFar + zNear - range / distanceToTarget) / (zFar - zNear)
+        // Convert to normalized coordinates [0, 1].
+        return Float((fragmentDepth + 1.0) / 2.0)
+    }
+    
+    private func viewSpaceBoundingBox(fromNormalizedImageBoundingBox imageBoundingBox: CGRect,
+                                      usingDisplayTransform displayTransorm: CGAffineTransform) -> CGRect {
+        // Transform into normalized view coordinates
+        let viewNormalizedBoundingBox = imageBoundingBox.applying(displayTransorm)
+        // The affine transform for view coordinates
+        let t = CGAffineTransform(scaleX: screenSize.width, y: screenSize.height)
+        // Scale up to view coordinates
+        return viewNormalizedBoundingBox.applying(t)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        guard let capturedImage = sceneView.session.currentFrame?.capturedImage else { return }
+        
+        // Take into account the different aspect ratios
+        let capturedImageAspectRatio = Float(CVPixelBufferGetWidth(capturedImage)) / Float(CVPixelBufferGetHeight(capturedImage))
+        let screenAspectRatio = Float(screenSize.height / screenSize.width)
+        quadNode?.aspectRatioAdjustment = screenAspectRatio / capturedImageAspectRatio
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: capturedImage,
+                                                        orientation: .leftMirrored,
+                                                        options: [:])
+        do {
+            // Perform the requests to detect and segment the cars
+            try imageRequestHandler.perform([objectDetectionRequest, segmentationRequest])
+        } catch {
+            print("Failed to perform image request. \(error)")
+        }
+    }
+}
